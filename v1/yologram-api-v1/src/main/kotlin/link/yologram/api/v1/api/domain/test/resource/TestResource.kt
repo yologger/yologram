cff036f4f492cf66225c36c6f42d8f57e6ffc090
yologger <yologger1013@gmail.com>
2026-05-03 18:28:58 +0900

    chore: add echo endpoint to return client request information

--- v1/yologram-api-v1/src/main/kotlin/link/yologram/api/v1/api/domain/test/resource/TestResource.kt
+++ v1/yologram-api-v1/src/main/kotlin/link/yologram/api/v1/api/domain/test/resource/TestResource.kt
@@ -1,5 +1,6 @@
 package link.yologram.api.v1.api.domain.test.resource
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,5 +12,24 @@ class TestResource {
     @GetMapping
     fun index(): String {
         return "/v1/test"
+    }
+
+    @GetMapping("echo")
+    fun echo(request: HttpServletRequest): Map<String, Any?> {
+        val headers = mutableMapOf<String, String>()
+        val headerNames = request.headerNames
+        while (headerNames.hasMoreElements()) {
+            val headerName = headerNames.nextElement()
+            headers[headerName] = request.getHeader(headerName)
+        }
+
+        return mapOf(
+            "ip" to request.remoteAddr,
+            "userAgent" to request.getHeader("User-Agent"),
+            "method" to request.method,
+            "uri" to request.requestURI,
+            "protocol" to request.protocol,
+            "headers" to headers
+        )
     }
 }
