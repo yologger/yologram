a9dfad04fac9d87e9fcecfb066703dab05e8a943
yologger <yologger1013@gmail.com>
2026-06-09 22:47:20 +0900

    [yologram-web-v2] style: 설정 페이지 색상 파란색 계열로 통일, ConfigProvider 추가

--- yologram-web-v2/src/app/providers.tsx
+++ yologram-web-v2/src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConfigProvider } from 'antd'
 import { useState } from 'react'
 import AuthGate from '../components/auth/AuthGate'
 
@@ -8,10 +9,12 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthGate>
-        {children}
-      </AuthGate>
-    </QueryClientProvider>
+    <ConfigProvider theme={{ token: { colorPrimary: '#1677ff' } }}>
+      <QueryClientProvider client={queryClient}>
+        <AuthGate>
+          {children}
+        </AuthGate>
+      </QueryClientProvider>
+    </ConfigProvider>
   )
 }
