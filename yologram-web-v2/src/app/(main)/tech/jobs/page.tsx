3ebdb755d8fb0783f0776500bedd6af5db46867a
yologger <yologger1013@gmail.com>
2026-06-27 22:49:58 +0900

    [yologram-web-v2] feat: 기술 채용(jobs) 탭 준비 중 처리
    - tech/jobs/page.tsx를 ComingSoon 렌더로 교체(tech 서브탭 헤더는 유지, 채용 콘텐츠만 준비 중)
    - 기존 page는 주석 보존(구현 시 원복)

--- yologram-web-v2/src/app/(main)/tech/jobs/page.tsx
+++ yologram-web-v2/src/app/(main)/tech/jobs/page.tsx
@@ -1,3 +1,11 @@
+import ComingSoon from '@/components/common/ComingSoon'
+
+// 채용 준비 중: 구현 시작 시 ComingSoon 제거하고 아래 기존 page를 복구한다.
+export default function TechJobs() {
+  return <ComingSoon />
+}
+
+/*
 'use client'
 
 import { Typography } from 'antd'
@@ -5,3 +13,4 @@ import { Typography } from 'antd'
 export default function TechJobs() {
   return <Typography.Text>기술 채용</Typography.Text>
 }
+*/
